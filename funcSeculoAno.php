965fc640d2a68ca6435964db646126b67ca4d308
hecjairon <hecjairon@hotmail.com>
2021-08-07 02:29:33 -0300

    check update all files

--- funcSeculoAno.php
+++ funcSeculoAno.php
@@ -1,9 +1,9 @@
 <?php
 
-function SeculoAno($ano){
+function SeculoAno(int $ano){
+    if($ano < 0) return false;
     $calculaSeculo = $ano/100;
     $resto = $ano%100;
     $seculo = (int)$seculo + (($resto>0) ? 1 :0);
     return $seculo;
-
 }
